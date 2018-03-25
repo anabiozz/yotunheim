62b26d4ad5bdb7838977e2645596434f8082d2c4
almex <alexeybezrukov2@gmail.com>
2018-03-26 00:06:08 +0300

    added time to graphics x line

--- frontend/dashboard/components/ServersCallGrid.js
+++ frontend/dashboard/components/ServersCallGrid.js
@@ -7,16 +7,17 @@ export default class ChartCell extends React.Component {
     }
 
     _renderObject(chartData){
+       
         return Object.entries(chartData).map(([key, value], i) => {
 			return (
 				<div key={i}>
-					<LineChart width={600} height={300} data={value} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                    <XAxis dataKey={key + ' usage'}/>
-                    <YAxis domain={[0, 100]} />
-                    <CartesianGrid strokeDasharray='3 3'/>
-                    <Tooltip/>
-                    <Legend />
-                    <Line type='monotone' dataKey='payload' name={key + ' usage'} stroke='#8884d8' activeDot={{r: 8}}/>
+					<LineChart width={600} height={300} data={value} margin={{top: 10, right: 30, left: 30, bottom: 10}}>
+                        <XAxis dataKey='timestamp'/>
+                        <YAxis domain={[0, 100]} />
+                        <CartesianGrid strokeDasharray='3 3'/>
+                        <Tooltip/>
+                        <Legend />
+                        <Line type='monotone' dataKey='payload' name={key + ' usage'} stroke='#8884d8' activeDot={{r: 8}}/>
                     </LineChart>
 				</div>
 			)
